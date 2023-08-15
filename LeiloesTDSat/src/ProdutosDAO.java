52bcdf42bd771a6f80590e7415562317705e09e8
Sabrina <sabrinaslimap@gmail.com>
2023-08-15 12:04:39 -0300

    inclusão da funcionalidade listagem de produtos

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -37,15 +37,55 @@ public class ProdutosDAO {
             JOptionPane.showMessageDialog(null, "O produto foi cadastrado com sucesso!");
 
         } catch (SQLException e) {
-            
+
             JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + e.getMessage());
-        
-        } 
-       
-        
+
+        }
+
     }
 
     public ArrayList<ProdutosDTO> listarProdutos() {
+
+        conn = new conectaDAO().connectDB();
+
+        try {
+            String query = "SELECT * FROM produtos";
+            prep = conn.prepareStatement(query);
+            resultset = prep.executeQuery();
+
+            listagem.clear();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+
+            }
+
+        } catch (SQLException erro) {
+
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + erro.getMessage());
+
+        } finally {
+
+            try {
+                if (resultset != null) {
+                    resultset.close();
+                }
+                if (prep != null) {
+                    prep.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+
+            }
+        }
 
         return listagem;
     }
