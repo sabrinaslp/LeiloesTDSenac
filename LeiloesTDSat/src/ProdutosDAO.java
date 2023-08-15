d86f6622cfae7228cc097d503102358a39adebd1
Sabrina <sabrinaslimap@gmail.com>
2023-08-15 11:45:31 -0300

    inclusão da funcionalidade cadastrar produto

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -7,36 +7,40 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+
+        conn = new conectaDAO().connectDB();
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        try {
+            String query = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+            prep = conn.prepareStatement(query);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+
+            prep.executeUpdate();
+
+        } catch (SQLException e) {
+
+        }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
 }
-
