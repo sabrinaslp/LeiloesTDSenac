05dc8feee7cbfa9af91e6096502af1bfde6bf0af
Sabrina <sabrinaslimap@gmail.com>
2023-08-17 18:56:12 -0300

    ajuste funcionalidade do botão voltar em vendasVIEW

--- LeiloesTDSat/src/vendasVIEW.java
+++ LeiloesTDSat/src/vendasVIEW.java
@@ -110,7 +110,7 @@ public class vendasVIEW extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
